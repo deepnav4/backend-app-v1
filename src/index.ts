8915b1fe5b511187de714d6b68dde09a23d6903a
deepnav4 <workwithdeepnav@gmail.com>
2026-01-16 13:37:32 +0530

    add-auto-deploy-to-ec2

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Hello, World! CI/CD is working! ');
 });
 
 app.listen(port, () => {
